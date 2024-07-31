e025f32bb648f3ec7943d48daf4db6e287e2f1b7
bo zhu <18503049065@163.com>
2024-07-31 17:10:22 +0800

    Initial Commit

---
+++ test demo5/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  test demo5
+//
+//  Created by Bo Zhu on 2024/7/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+
+// apple
---
+++ test demo5/test_demo5App.swift
@@ -0,0 +1,20 @@
+//
+//  test_demo5App.swift
+//  test demo5
+//
+//  Created by Bo Zhu on 2024/7/31.
+//
+
+import SwiftUI
+
+@main
+struct test_demo5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
+// apple
